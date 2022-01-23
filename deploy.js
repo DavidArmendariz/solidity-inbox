36ba0c9ca8f5bf60e68f0b3fa080da6c40cafa8e
David Armendariz <david.adrian.a.p@gmail.com>
2022-01-23 13:30:44 -0500

    Fixed deployment code

--- deploy.js
+++ deploy.js
@@ -13,8 +13,7 @@ const deploy = async () => {
   try {
     const accounts = await web3.eth.getAccounts();
     console.log(`Deploying from account: ${accounts[0]}`);
-    const result = await web3.eth
-      .Contract(JSON.parse(Inbox.interface))
+    const result = await new web3.eth.Contract(JSON.parse(Inbox.interface))
       .deploy({ data: Inbox.bytecode, arguments: ['Hello World!'] })
       .send({ gas: 1000000, from: accounts[0] });
     console.log(`Contract Address: ${result.options.address}`);
